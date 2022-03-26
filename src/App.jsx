9e832f5f1eaa5ddec07d6147860e6d29ca723f0e
Julian Martinez <julian.fmartinez@gmail.com>
2022-03-26 19:10:13 -0300

    Se inicia el routing del proyecto con react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './layout/Layout'
+import Inicio from './pages/Inicio'
+import NuevoCliente from './pages/NuevoCliente'
+import EditarCliente from './pages/EditarCliente'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {
+          /* 
+          Todas las los Route dentro de Route path="/clientes" pertenecen a este grupo
+          /clientes/nuevo
+          /clientes/editar/:id
+          */
+        }
+        <Route path="/clientes" element={<Layout />}>
+          <Route index element={<Inicio/>}/>
+          <Route path="nuevo" element={<NuevoCliente/>}/>
+          <Route path="editar/:id" element={<EditarCliente/>}/>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ src/pages/EditarCliente.jsx
@@ -0,0 +1,11 @@
+
+
+const EditarCliente = () => {
+  return (
+    <div>
+        <h1>Editar cliente</h1>
+    </div>
+  )
+}
+
+export default EditarCliente
